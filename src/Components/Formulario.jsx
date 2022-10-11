39b89b8b3dc8c3cecf32e25281ff866fe3443a89
Gerardo <a21110179@ceti.mx>
2022-10-10 23:24:10 -0500

    Arreglé el formulario para centrarlo

--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -5,12 +5,22 @@ export const Formulario = () => {
     
         <div className="container">
 
-        <form className="form">
+        <form className="form text-center">
 
             <label className="form-label" htmlFor="Tamaño">Tamaño de la matriz:</label>
+            
+            <div className="w-25 m-auto">
+
             <input className="form-control" type="number" />
 
-            <button type="submit" className="btn btn-primary mt-2 mb-2 col-12">Enviar</button>
+            </div>
+
+            <div className="w-25 m-auto">
+
+            <button type="submit" className="btn btn-outline-dark mt-2 mb-2 col-12">Enviar</button>
+
+            </div>
+
             </form>
 
         </div>
