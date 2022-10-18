4c592d5fa907832df7c431aeebdac66f718e4148
Cristian Bernachi <a21110180@ceti.mx>
2022-10-18 16:08:33 -0500

    Añadi color a nodos y sus respectivas animaciones

--- src/Components/Column.jsx
+++ src/Components/Column.jsx
@@ -7,7 +7,11 @@ export const Column = ({row, col, isFinish, isStart, isWall, onMouseDown}) => {
   ? 'node-start'
   : isWall
   ? 'node-wall'
-  : '';
+  : col
+  ?'node'
+  : row
+  ? 'node'
+  : 'node';
 
   return (
 
@@ -15,7 +19,7 @@ export const Column = ({row, col, isFinish, isStart, isWall, onMouseDown}) => {
     
         <div 
           style={{height: 150}} 
-          className={`bg-dark col m-2 ${extraClassName}`}
+          className={` col m-2 ${extraClassName}`}
           id={`node-${row}-${col}`}
           onMouseDown={() => onMouseDown(row, col)}
         >
