b6722aecb0728a019219d0c86caa90b1f1653276
re150 <a21110175@ceti.mx>
2022-10-19 22:35:53 -0500

    Agregre la funcion dijkstra en la cual se evaluan el grid y los nodo para saber los status de los mismo y cual son pared y el valor de cada nodo del grid

--- src/Algoritmo/dijkstra.js
+++ src/Algoritmo/dijkstra.js
@@ -1,8 +1,27 @@
+
 export const dijkstra = (completeGrid, startNode, finishNode) => {
 
-    return "Hola que hace";
+    const visitedNodesInOrder = [];
+    startNode.distance = 0;
+    const unvisitedNodes = getAllNodes(completeGrid);
+
+      while (!!unvisitedNodes.length) {
+      sortNodesByDistance(unvisitedNodes);
+
+      const closestNode = unvisitedNodes.shift();
+
+      if (closestNode.isWall) continue;
+
+      if (closestNode.distance === Infinity) return visitedNodesInOrder;
+      closestNode.isVisited = true;
+      visitedNodesInOrder.push(closestNode);
+      if (closestNode === finishNode) return visitedNodesInOrder;
+      updateUnvisitedNeighbors(closestNode, completeGrid);
+    }
 
 }
+
+
 
 export function getNodesInShortestPathOrder(finishNode) {
   const nodesInShortestPathOrder = [];
